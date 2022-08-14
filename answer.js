5d14c13f463291695ce80ca72201e765cb2da79d
mnubhan <76425959+mnubhan@users.noreply.github.com>
2022-08-14 14:24:28 +0800

    Create answer.js

---
+++ answer.js
@@ -0,0 +1,24 @@
+function removeSmallest(numbers) {
+  numbers = numbers.slice(0);
+  const min = Math.min(...numbers);
+  numbers.splice(numbers.indexOf(min), 1);
+  return numbers;
+}
+
+function removeSmallest(numbers) {
+  let indexOfMin = numbers.indexOf(Math.min(...numbers));
+  return [...numbers.slice(0, indexOfMin), ...numbers.slice(indexOfMin + 1)];
+}
+
+function removeSmallest(numbers) {
+  const min = Math.min.apply(this, numbers);
+  return numbers.filter((num, idx, arr) => idx !== arr.indexOf(min));
+}
+
+const removeSmallest = numbers => numbers.filter((n,i) => i !== numbers.indexOf(Math.min(...numbers)));
+
+function removeSmallest(numbers) {
+  numbers = numbers.slice(0);
+  const min = Math.min(...numbers);
+  numbers.splice(numbers.indexOf(min), 1);
+  return numbers;
